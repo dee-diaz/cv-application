86e3b4daaf5fdfcb4d27fd2104f3d680d2a7a7dc
dee-diaz <diazthediablo@yahoo.com>
2026-01-15 12:10:23 -0300

    feat: render Header component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header.jsx';
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
 
 export default App;
