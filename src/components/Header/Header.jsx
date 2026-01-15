bfc27e2a8975c9c0cc0c2c48a9886b9daa279b83
dee-diaz <diazthediablo@yahoo.com>
2026-01-15 11:38:22 -0300

    feat: add basic Header structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Turn your CV into a yes</h1>
+    </header>
+  )
+}
