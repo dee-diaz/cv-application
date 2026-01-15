d26a19270c2c0112d605bd81713fe9a0a97b9d4c
dee-diaz <diazthediablo@yahoo.com>
2026-01-15 12:09:40 -0300

    feat: add download CTA to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './Header.css';
+import DownloadButton from '../Button/DownloadButton.jsx';
 
 export default function Header() {
   return (
     <header>
       <h1>Turn your CV into a yes</h1>
+      <DownloadButton />
     </header>
   )
 }
