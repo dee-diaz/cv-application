5fc82492bc4db1de74e6a363ad871475e12ec9aa
dee-diaz <diazthediablo@yahoo.com>
2026-01-15 17:19:47 -0300

    feat: add Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,11 @@ import './Header.css';
 import DownloadButton from '../Button/DownloadButton.jsx';
 
 export default function Header() {
+  const titleText = window.innerWidth < 992 ? 'CV' : 'Turn your CV into a yes';
   return (
-    <header>
-      <h1>Turn your CV into a yes</h1>
+    <header className="header">
+      <h1>{titleText}</h1>
       <DownloadButton />
     </header>
-  )
+  );
 }
