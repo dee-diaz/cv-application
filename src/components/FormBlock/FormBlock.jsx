512fbd412fedf8e3b888f21fcddfafda53aef9dc
dee-diaz <diazthediablo@yahoo.com>
2026-01-16 20:27:31 -0300

    feat: implement and style FormBlock component

--- src/components/FormBlock/FormBlock.jsx
+++ src/components/FormBlock/FormBlock.jsx
@@ -1,1 +1,41 @@
 import './FormBlock.css';
+import { useState } from 'react';
+
+export default function FormBlock({ title, inputs }) {
+  const formId = title.toLowerCase().replace(' ', '-');
+  const inputsArr = Object.values(inputs);
+  return (
+    <div className="form-block-wrapper">
+      <form className={formId} name={formId} id={formId}>
+        {inputsArr.map((input) => (
+          <Input
+            key={input.id}
+            as={input.as}
+            label={input.label}
+            type={input.type}
+            id={input.id}
+            placeholder={input.placeholder}
+          />
+        ))}
+      </form>
+    </div>
+  );
+}
+
+function Input({ as: Component = 'input', label, type, id, placeholder }) {
+  const [value, setValue] = useState('');
+
+  return (
+    <label>
+      {label}
+      <Component
+        type={type}
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </label>
+  );
+}
