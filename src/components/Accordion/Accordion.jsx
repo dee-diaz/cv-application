79fab1ac801eb9b3222df0856ad814efe37fb5ca
dee-diaz <diazthediablo@yahoo.com>
2026-01-18 09:20:43 -0300

    feat(accordion): add Accordion component and styles)

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,1 +1,12 @@
 import './Accordion.css';
+
+export default function Accordion({ headerTitle, children }) {
+  return (
+    <details className="accordion">
+      <summary>{headerTitle}</summary>
+      <div className="content">
+        <div className="inner">{children}</div>
+      </div>
+    </details>
+  );
+}
