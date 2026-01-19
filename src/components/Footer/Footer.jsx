e22287488d6b3b371ecc85ca66b805119c0fa31e
dee-diaz <diazthediablo@yahoo.com>
2026-01-19 12:40:09 -0300

    Update Footer component styling and layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Created by{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Diana Diaz (opens in a new tab)"
+          href="https://github.com/dee-diaz"
+        >
+          Diana Diaz
+        </a>
+      </p>
+    </footer>
+  );
+}
