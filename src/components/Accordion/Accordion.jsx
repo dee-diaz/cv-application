c5b349a6a1c41fa2390a0b3b853ccfc79cbddc66
dee-diaz <diazthediablo@yahoo.com>
2026-01-20 13:25:51 -0300

    feat(Accordion): add collapse/expand state

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './Accordion.css';
 
 export default function Accordion({ headerTitle, children }) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  function handleClick() {
+    setIsCollapsed(!isCollapsed);
+  }
+
   return (
     <details className="accordion">
-      <summary>{headerTitle}</summary>
+      <summary onClick={handleClick}>{headerTitle}</summary>
       <div className="content">
         <div className="inner">{children}</div>
       </div>
